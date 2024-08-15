133de9310ca0e07122d26487443d9e3570297088
Alexey Germanov <aleck.germanov@yandex.ru>
2024-08-15 19:08:01 +0300

    ts error fix

--- hooks/useComparisonLinks.ts
+++ hooks/useComparisonLinks.ts
@@ -12,12 +12,12 @@ export const useComparisonLinks = () => {
   const { lang, translations } = useLang()
   const pathname = usePathname()
 
+
   const availableProductLinks = useMemo(
     () =>
       [
-        ...new Set(
-          currentComparisonByAuth.map((item) => item.characteristics.type)
-        ),
+        // @ts-ignore
+        ...new Set(currentComparisonByAuth.map((item) => item.characteristics.type)),
       ].map((type) => ({
         title: (translations[lang].comparison as { [index: string]: string })[
           type
